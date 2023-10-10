6ed219a6821e9e11a018aad76168775fe4f08f6c
John Francis <john9francis@gmail.com>
2023-10-10 10:43:01 -0600

    created custom build script to make the compilation super easy

---
+++ build.ps1
@@ -0,0 +1,18 @@
+# Change directory to the build folder
+cd build
+
+# Run CMake
+cmake ..
+
+# Build the project using MSBuild
+msbuild Test_Calorimeter.sln
+
+# Change directory to the release folder
+cd release
+
+# Run the executable
+.\Test_Calorimeter
+
+# Go back to the root directory
+cd ..
+cd ..
