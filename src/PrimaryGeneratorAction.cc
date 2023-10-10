727b3329f49abfd53ac3b0be063f08f16dc0f21a
John Francis <john9francis@gmail.com>
2023-10-10 10:22:45 -0600

    cleaned up primarygeneratoraction

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -13,25 +13,23 @@
 namespace TC
 {
 	PrimaryGeneratorAction::PrimaryGeneratorAction() {
-		// set up particle gun
-		G4int nParticles = 1;
-		fParticleGun = new G4ParticleGun(nParticles);
+		fParticleGun = new G4ParticleGun();
 
 		// define particle properties
-		const G4String& particleName = "e-";
+		G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+		const G4String& particleName = "gamma";
+		G4ParticleDefinition* particle = particleTable->FindParticle(particleName);
+		fParticleGun->SetParticleDefinition(particle);
 
 		G4ThreeVector momentumDirection = G4ThreeVector(0, 0, 1);
-
-		// default particle kinematic
-		G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
-		G4ParticleDefinition* particle
-			= particleTable->FindParticle(particleName);
-		fParticleGun->SetParticleDefinition(particle);
 		fParticleGun->SetParticleMomentumDirection(momentumDirection);
 
-		// (test) hardcode the energy and we'll change it in mac files
-		G4double energy = 6 * MeV;
+		G4double energy = 5 * MeV;
 		fParticleGun->SetParticleEnergy(energy);
+
+		G4ThreeVector position = G4ThreeVector(0, 0, -5 * cm);
+		fParticleGun->SetParticlePosition(position);
+
 
 	}
 
@@ -41,32 +39,6 @@ namespace TC
 
 	void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 	{
-		// Randomize x and y starting point within a 1 mm diameter
-		G4double radius = .5 * mm;
-
-		// generate random x and y positions within that radius
-		double x, y;
-
-		// to avoid using slow methods like sin and cos,
-		// we generate random values in a cube and regect the ones
-		// outside of a circle. This way 
-		do {
-			x = G4UniformRand() * (2.0 * radius) - radius;
-			y = G4UniformRand() * (2.0 * radius) - radius;
-		} while (x * x + y * y > radius * radius);
-
-		G4ThreeVector position = G4ThreeVector(x, y, -5 * cm);
-		fParticleGun->SetParticlePosition(position);
-
-		// randomize energy with a .127 MeV std:dev gaussean distribution
-		//G4double meanEnergy = 6. * MeV;
-		//G4double stdDev = .127 * MeV;
-		//G4double energy = G4RandGauss::shoot(meanEnergy, stdDev);
-		//
-		//fParticleGun->SetParticleEnergy(energy);
-
-
-		// satisfy "generate primaries" here.
 		fParticleGun->GeneratePrimaryVertex(event);
 	}
 }
