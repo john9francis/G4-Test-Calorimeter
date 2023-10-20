73c5807c336f2a8ca320e4ffd145fc53669c15e0
John Francis <john9francis@gmail.com>
2023-10-20 00:41:54 -0600

    Created RunAction class

---
+++ include/RunAction.hh
@@ -0,0 +1,17 @@
+#ifndef TC_RUN_ACTION_HH
+#define TC_RUN_ACTION_HH
+
+#include "G4UserRunAction.hh"
+
+namespace TC {
+	class RunAction : public G4UserRunAction {
+	public:
+		RunAction();
+		~RunAction();
+
+		void BeginOfRunAction(const G4Run*) override;
+		void EndOfRunAction(const G4Run*) override;
+	};
+}
+
+#endif // !TC_RUN_ACTION_HH
---
+++ src/RunAction.cc
@@ -0,0 +1,6 @@
+
+#include "RunAction.hh"
+
+namespace TC {
+
+}
