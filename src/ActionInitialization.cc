74dbea5fc6cdbb183129a0596374c50c7a9394c6
John Francis <john9francis@gmail.com>
2023-10-20 00:48:38 -0600

    Registered the runAction in actionInit, works correctly

--- src/ActionInitialization.cc
+++ src/ActionInitialization.cc
@@ -1,13 +1,15 @@
 #include "ActionInitialization.hh"
 
 #include "PrimaryGeneratorAction.hh"
+#include "RunAction.hh"
 
 namespace TC {
 	void ActionInitialization::Build() const {
 		SetUserAction(new PrimaryGeneratorAction);
+		SetUserAction(new RunAction);
 	}
 
 	void ActionInitialization::BuildForMaster() const {
-
+		SetUserAction(new RunAction);
 	}
 }
